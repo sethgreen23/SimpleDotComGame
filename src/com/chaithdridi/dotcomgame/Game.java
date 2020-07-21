4b77fbf5a31840cb115ae85799d474a4cf4478bb
sethgreen23 <dridi.chaith@gmail.com>
2020-07-21 19:05:16 +0100

    second commit

--- src/com/chaithdridi/dotcomgame/Game.java
+++ src/com/chaithdridi/dotcomgame/Game.java
@@ -21,9 +21,12 @@ public class Game {
 		String userGuess="",result = "";
 		boolean active = true;
 		DotCom dot = new DotCom();
+		//create the first random location between 0 and 5
 		int random = (int) (Math.random()*5);
+		//create the location and set it 
 		int[] location = {random,random+1,random+2};
 		dot.setLocation(location);
+		//get user input and start the game
 		String prompt = "enter your number :";
 		InputHelper console = new InputHelper();
 		while(active) {
@@ -32,6 +35,7 @@ public class Game {
 			if(result!=null) {
 				++numOfGuess;
 			}
+			//we get out from the game if we have a kill
 			if(result == "kill") {
 				System.out.println("You took "+numOfGuess+" guesses");
 				active = false;
